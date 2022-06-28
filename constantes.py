e3a77b29f2b008faa840a7fcb5ad32679164ad95
Patrick Souza <tricksouza82@gmail.com>
2022-06-28 10:03:49 -0400

    first commit

---
+++ constantes.py
@@ -0,0 +1,6 @@
+largura_min = 30  # Largura minima do retangulo
+altura_min = 50  # Altura minima do retangulo
+offset = 1  # Erro permitido entre pixel
+pos_linha = 490  # Posição da linha de contagem
+delay = 60  # FPS do vídeo
+detec = []
